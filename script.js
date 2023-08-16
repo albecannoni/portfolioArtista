2c20c0ef0f5cd251bb2aecf0b41682cd31510f3d
albecannoni <alberto.cannoni@outlook.it>
2023-08-16 11:19:00 +0200

    update design + script per produzioni

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
+// Chiamata AJAX per ottenere i dati
+let urlAPI = 'db/db.json';
+let xhr = new XMLHttpRequest();
+xhr.open('GET', urlAPI);
+xhr.send();
 
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        let produzioni = JSON.parse(xhr.responseText);
 
-function test() {
-    alert("TESTP");
-}
+        // Inizializza un array per i dati ottenuti
+        let produzioniArray = [];
 
-function init() {
-    test();
-}
+        // Pusha i dati ottenuti nell'array
+        produzioni.forEach(element => {
+            produzioniArray.push({
+                titolo: element.titolo,
+                tipo: element.tipo,
+                descrizione: element.descrizione
+            });
+        });
 
-// Otteniamo il riferimento all'elemento .hamburger e alla finestra modale
-const hamburgerButton = document.querySelector('.hamburger');
-const modalOverlay = document.querySelector('.modal-overlay');
-const closeModalButton = document.querySelector('.close-modal');
-const modal = document.querySelector('.modal');
+        // Stampa i dati nell'HTML come lista
+        let productionList = document.querySelector('.production-list');
 
-// Aggiungiamo un gestore di eventi per aprire la finestra modale
-hamburgerButton.addEventListener('click', () => {
-    modalOverlay.style.display = 'block';
-    modal.style.left = '50%';
-});
+        produzioniArray.forEach(produzione => {
+            let listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <h2>${produzione.titolo}</h2>
+                <p>${produzione.tipo}</p>
+                <p>${produzione.descrizione}</p>
+            `;
+            productionList.appendChild(listItem);
 
-// Aggiungiamo un gestore di eventi per chiudere la finestra modale
-closeModalButton.addEventListener('click', () => {
+            // Aggiungi gestore di eventi al click sulla <li>
+            listItem.addEventListener('click', () => {
+                alert(`Hai cliccato su:\nTitolo: ${produzione.titolo}\nTipo: ${produzione.tipo}\nDescrizione: ${produzione.descrizione}`);
+            });
+        });
+
+        // Funzione di inizializzazione
+        init();
+    }
+};
+
+// Funzione di chiusura della finestra modale
+function closeModal() {
+    const modalOverlay = document.querySelector('.modal-overlay');
+    const modal = document.querySelector('.modal');
     modalOverlay.style.display = 'none';
     modal.style.left = '100%';
-});
+}
 
+// Funzione di inizializzazione
+function init() {
+    // Otteniamo il riferimento all'elemento .hamburger e alla finestra modale
+    const hamburgerButton = document.querySelector('.hamburger');
+    const closeModalButton = document.querySelector('.close-modal');
+    
+    // Aggiungiamo un gestore di eventi per aprire la finestra modale
+    hamburgerButton.addEventListener('click', () => {
+        const modalOverlay = document.querySelector('.modal-overlay');
+        const modal = document.querySelector('.modal');
+        modalOverlay.style.display = 'block';
+        modal.style.left = '50%';
+    });
 
+    // Aggiungiamo un gestore di eventi per chiudere la finestra modale
+    closeModalButton.addEventListener('click', closeModal);
+}
+
+// Aggiungiamo un gestore di eventi per il caricamento della pagina
 window.addEventListener("load", init);
